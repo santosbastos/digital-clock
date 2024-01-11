ea79480c67479986c22d11b0c52ce2910365b768
codenet.dev <152228077+santosbastos@users.noreply.github.com>
2024-01-11 10:33:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,19 @@ const secondE1 = document.getElementById("seconds");
 const ampmE1 = document.getElementById("ampm");
 
 function updateClock() {
-  let h = new Date().getHours();
-  let m = new Date().getMinutes();
-  let s = new Date().getSeconds();
-  let ampm = "AM";
+ let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+let ampm = "AM";
 
-  // ...
 if (h >= 12) {
   ampm = "PM";
   if (h > 12) {
     h = h - 12;
   }
-} else {
-  ampm = "AM";
 }
+console.log("h:", h);
+console.log("ampm:", ampm);
 
 h = h < 10 ? "0" + h : h;
 m = m < 10 ? "0" + m : m;
